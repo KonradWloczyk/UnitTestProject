167149a05275b5b6250850db88a7d3ceb6d50ad2
Konrad Włoczyk <konrad_wloczyk@o2.pl>
2020-05-09 14:14:45 +0200

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eecbb20f-9587-4373-80b6-8d37be49a5ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO.IOException;
+
+namespace UnitTestProject
+{
+
+    [TestClass]
+    public class LeapYear
+    {
+        
+        [TestMethod]
+        public void Leap_Year_Test_2018()
+        {
+            string result = IsLeapYear(2018);
+            StringAssert.Equals(result, "No");
+        }
+
+        [TestMethod]
+        public void Leap_Year_Test_2020()
+        {
+            string result = IsLeapYear(2020);
+            StringAssert.Equals(result, "Yes");
+        }
+        public string IsLeapYear(int givenYear)
+        {
+
+            int nYear, nFhun, nHun, nFour, nEven;
+            string sFhun, sHun, sFour;
+            nYear = givenYear;
+
+            nFhun = nYear % 400;
+            if (nFhun == 0)
+            {
+                sFhun = "Divisible by 400: Yes";
+            }
+            else
+            {
+                sFhun = "Divisible by 400: No";
+            }
+            nHun = nYear % 100;
+            if (nHun == 0)
+            {
+                sHun = "Divisible by 100: Yes";
+            }
+            else
+            {
+                sHun = "Divisible by 100: No";
+            }
+            nFour = nYear % 4;
+            if (nFour == 0)
+            {
+                sFour = "Divisible by 100: Yes";
+            }
+            else
+            {
+                sFour = "Divisible by 100: No";
+            }
+
+            nEven = nYear % 2;
+            Console.WriteLine("Year: " + nYear);
+            Console.WriteLine(sFour);
+            Console.WriteLine(sHun);
+            Console.WriteLine(sFhun);
+
+            if ((nEven == 0) && (nFour == 0 ) && (nHun == 0) && (nFhun ==0))
+            {
+                Console.WriteLine("Leap Year: Yes");
+                return "Yes";
+            }
+            else if ((nEven == 0) && (nFour == 0) && (nHun != 0) && (nFhun !=0))
+            {
+                Console.WriteLine("Leap Year: Yes");
+                return "Yes";
+            }
+
+            else if ((nEven == 0) && (nFour == 0) && (nHun == 0) && (nFhun != 0))
+            {
+                Console.WriteLine("Leap Year: No");
+                return "No";
+            }
+
+            else if ((nEven == 0) && (nFour == 0) && (nHun != 0) && (nFhun == 0))
+            {
+                Console.WriteLine("Leap Year: No");
+                return "No";
+            }
+            return "No";
+        }
+        
+
+    }
+}
